8761eef16788d303044d3551ccbef95129586ec6
Ruoyi Wang <endvroy@gmail.com>
2019-03-29 17:11:55 -0700

    added script for easier testing setup

---
+++ prep_test_grammar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp BashLexer.g4 BashParser.g4 /tmp/bash
+cd /tmp/bash
+export CLASSPATH=".:/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH"
+antlr4='java -jar /usr/local/lib/antlr-4.7.2-complete.jar'
+$antlr4 BashLexer.g4 BashParser.g4
+javac Bash*.java
+#grun Bash cmd -gui
