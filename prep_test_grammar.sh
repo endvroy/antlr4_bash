3d9c1f5819f162df893f8b98244d5c739e5cb398
Ruoyi Wang <endvroy@gmail.com>
2019-04-15 23:46:27 -0700

    now also copies the test suite

--- prep_test_grammar.sh
+++ prep_test_grammar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mkdir -p /tmp/bash
 cp BashLexer.g4 BashParser.g4 /tmp/bash
+cp tests.txt /tmp/bash
 cd /tmp/bash
 export CLASSPATH=".:/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH"
 antlr4='java -jar /usr/local/lib/antlr-4.7.2-complete.jar'
