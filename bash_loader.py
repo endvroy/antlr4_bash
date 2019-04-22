6c967bf8eb9546fad4545c163055fee302a50afe
Ruoyi Wang <endvroy@gmail.com>
2019-04-22 15:29:51 -0700

    slight clean up

--- bash_loader.py
+++ bash_loader.py
@@ -1,15 +1,12 @@
 from antlr4 import *
-from BashLexer import BashLexer
-from BashParser import BashParser
-from io import StringIO
+from gen.BashLexer import BashLexer
 
 if __name__ == '__main__':
     l = input('$ ')
-    # f = StringIO(l)
     input_stream = InputStream(l)
     lexer = BashLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    # stream.fill()
-    # tokens = stream.tokens
-    parser = BashParser(stream)
-    tree = parser.cmd()
+    stream.fill()
+    tokens = stream.tokens
+    # parser = BashParser(stream)
+    # tree = parser.cmd()
